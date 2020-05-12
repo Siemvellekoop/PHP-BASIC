cc7665055c9cd46ca4f93b8f72609b9f2d02b72e
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-05-12 13:32:38 +0200

    Taak 6 AF

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$var1 = "Tom";
+$var2 = 1;
+$var3 = 1.2;
+$var4 = true;
+$var5 = null;
+
+echo gettype($var1);
+echo gettype($var2);
+echo gettype($var3);
+echo gettype($var4);
+echo gettype($var5);
+?> 
+
+</body>
+</html>

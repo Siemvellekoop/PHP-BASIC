587f6b887d9137d4134c7b1eb59ca75a4d92d562
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-05-13 11:14:44 +0200

    Taak 8 AF

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Siem";
+$voornaam2 = "Michael";
+$voornaam3 = "David";
+
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+?> 
+
+</body>
+</html>

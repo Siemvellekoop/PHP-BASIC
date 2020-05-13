84bebbf3c7eb96bdf6ded914e80a85a2904abace
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-05-13 11:16:42 +0200

    taak 9 AF

---
+++ 2-Variabelen-en-datatype/taak09/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam = "Siem";
+$leeftijd = 17;
+
+echo "Jouw naam is " . $voornaam . " en je bent " . $leeftijd . " jaar oud.";
+
+?>
+
+</body>
+</html>

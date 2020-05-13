21f42830d7bd8f9b3284158d63d86f9819d35063
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-05-13 12:34:26 +0200

    Taak 1 AF

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$getal1 = 10;
+$getal2 = 20;
+
+echo $getal1 + $getal2;
+echo "<br>";
+echo $getal1 - $getal2;
+echo "<br>";
+echo $getal1 / $getal2;
+echo "<br>";
+echo $getal1 * $getal2;
+
+?>
+</body>
+</html>

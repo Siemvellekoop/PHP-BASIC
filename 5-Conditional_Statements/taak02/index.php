88c71ab1cb1e1d2d9a69a5eb9cd32e3770cbaead
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-05-13 14:08:47 +0200

    taak 2 AF

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$test1 = 10;
+$test2 = 6;
+
+if($variabele1 == $variabele2) {
+    echo "gelijk";
+}
+if($variabele1 != $variabele2) {
+    echo "ongelijk";
+}
+if($variabele1 === $variabele2) {
+    echo "identiek";
+}
+if($variabele1 > $variabele2) {
+    echo "$variabele1 is groter dan. $variabele2";
+}
+if($variabele1 < $variabele2) {
+    echo "$variabele1 is kleiner dan. $variabele2";
+}
+if($variabele1 >= $variabele2) {
+    echo "$variabele1 is groter of gelijk aan. $variabele2";
+}
+if($variabele1 <= $variabele2) {
+    echo "$variabele1 is kleiner of gelijk aan. $variabele2";
+}
+
+?>
+</body>
+</html>

82d5259ab585ce1183ade73664edc703fd13e778
Siemvellekoop <Siemvellekoop@hotmail.nl>
2020-05-13 12:50:30 +0200

    Taak 1 AF

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$test1 = 10;
+$test2 = "Dit is tekst";
+
+var_dump($test1 == $test2);
+echo "<br>";
+var_dump($test1 === $test2);
+echo "<br>";
+var_dump($test1 != $test2);
+echo "<br>";
+var_dump($test1 > $test2);
+echo "<br>";
+var_dump($test1 < $test2);
+echo "<br>";
+var_dump($test1 >= $test2);
+echo "<br>";
+var_dump($test1 <= $test2);
+echo "<br>";
+var_dump($test1 <> $test2);
+
+
+?>
+</body>
+</html>
